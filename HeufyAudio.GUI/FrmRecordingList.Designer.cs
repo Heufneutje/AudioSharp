2efbb392d55806917898bf3f3e518c842bdf4696
Heufneutje <stefan.frijters@gmail.com>
2015-03-15 20:55:01 +0100

    Add an option to open the recordings folder

---
+++ HeufyAudio.GUI/FrmRecordingList.Designer.cs
@@ -0,0 +1,135 @@
+namespace HeufyAudio.GUI
+{
+    partial class FrmRecordingList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvRecordings = new System.Windows.Forms.ListView();
+            this.clmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnConvertToMp3 = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvRecordings
+            // 
+            this.lvRecordings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvRecordings.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmName,
+            this.clmType,
+            this.clmDate,
+            this.clmSize});
+            this.lvRecordings.GridLines = true;
+            this.lvRecordings.HideSelection = false;
+            this.lvRecordings.Location = new System.Drawing.Point(12, 12);
+            this.lvRecordings.Name = "lvRecordings";
+            this.lvRecordings.ShowGroups = false;
+            this.lvRecordings.Size = new System.Drawing.Size(573, 414);
+            this.lvRecordings.TabIndex = 0;
+            this.lvRecordings.UseCompatibleStateImageBehavior = false;
+            this.lvRecordings.View = System.Windows.Forms.View.Details;
+            this.lvRecordings.DoubleClick += new System.EventHandler(this.lvRecordings_DoubleClick);
+            // 
+            // clmName
+            // 
+            this.clmName.Text = "Name";
+            this.clmName.Width = 25;
+            // 
+            // clmType
+            // 
+            this.clmType.Text = "Type";
+            this.clmType.Width = 25;
+            // 
+            // clmDate
+            // 
+            this.clmDate.Text = "Date";
+            this.clmDate.Width = 25;
+            // 
+            // clmSize
+            // 
+            this.clmSize.Text = "Size";
+            this.clmSize.Width = 25;
+            // 
+            // btnConvertToMp3
+            // 
+            this.btnConvertToMp3.Location = new System.Drawing.Point(591, 41);
+            this.btnConvertToMp3.Name = "btnConvertToMp3";
+            this.btnConvertToMp3.Size = new System.Drawing.Size(99, 23);
+            this.btnConvertToMp3.TabIndex = 2;
+            this.btnConvertToMp3.Text = "Convert to MP3";
+            this.btnConvertToMp3.UseVisualStyleBackColor = true;
+            this.btnConvertToMp3.Click += new System.EventHandler(this.btnConvertToMp3_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(591, 12);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(99, 23);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Open folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // FrmRecordingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 438);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.btnConvertToMp3);
+            this.Controls.Add(this.lvRecordings);
+            this.Name = "FrmRecordingList";
+            this.Text = "Recordings";
+            this.Load += new System.EventHandler(this.FrmRecordingList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvRecordings;
+        private System.Windows.Forms.ColumnHeader clmName;
+        private System.Windows.Forms.ColumnHeader clmType;
+        private System.Windows.Forms.ColumnHeader clmDate;
+        private System.Windows.Forms.ColumnHeader clmSize;
+        private System.Windows.Forms.Button btnConvertToMp3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Button btnOpenFolder;
+    }
+}
---
+++ HeufyAudio.GUI/FrmRecordingList.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HeufyAudio.Core;
+using HeufyAudio.Translations;
+
+namespace HeufyAudio.GUI
+{
+    public partial class FrmRecordingList : Form
+    {
+        #region Fields & Properties
+        private string _RecordingPath;
+        private List<string> _Recordings;
+        private FrmProgressDialog _ProgressDialog;
+        #endregion
+
+        #region Constructor
+        public FrmRecordingList(string recordingPath)
+        {
+            InitializeComponent();
+            this._RecordingPath = recordingPath;
+            RefreshRecordings();
+        }
+        #endregion
+
+        #region Form Events
+        private void FrmRecordingList_Load(object sender, EventArgs e)
+        {
+            foreach (ColumnHeader column in lvRecordings.Columns)
+            {
+                column.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+        }
+        #endregion
+
+        #region Control Events
+        private void lvRecordings_DoubleClick(object sender, EventArgs e)
+        {
+            ListViewItem selectedItem = lvRecordings.SelectedItems[0];
+            Process.Start(_Recordings[selectedItem.Index]);
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            Process.Start(_RecordingPath);
+        }
+
+        private void btnConvertToMp3_Click(object sender, EventArgs e)
+        {
+            ListViewItem selectedItem = lvRecordings.SelectedItems[0];
+            string filePath = _Recordings[selectedItem.Index];
+
+            if (filePath.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show(Messages.GUIConvertAlreadyMP3, Messages.GUIConvertTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            _ProgressDialog = new FrmProgressDialog()
+            {
+                Task = "Converting WAV file to MP3...",
+                Progress = 0,
+                CanCancel = false
+            };
+            _ProgressDialog.Show();
+            backgroundWorker.RunWorkerAsync(_Recordings[selectedItem.Index]);
+        }
+        #endregion
+
+        #region Helper Functions
+        private void RefreshRecordings()
+        {
+            this._Recordings = new List<string>();
+            lvRecordings.Items.Clear();
+            string[] files = Directory.GetFiles(_RecordingPath);
+
+            foreach (string filePath in files)
+            {
+                FileInfo info = new FileInfo(filePath);
+                if (info.Extension.ToLower() != ".wav" && info.Extension.ToLower() != ".mp3")
+                    continue;
+
+                _Recordings.Add(filePath);
+                string formattedDate = info.CreationTime.ToString("yyyy-MM-dd HH:mm");
+                ListViewItem item = new ListViewItem(new[] { info.Name, info.Extension, formattedDate, BytesToString(info.Length) });
+                lvRecordings.Items.Add(item);
+            }
+        }
+
+        private string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+            if (byteCount == 0)
+                return "0 B";
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return String.Format("{0} {1}", Math.Sign(byteCount) * num, suf[place]);
+        }
+        #endregion
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            MP3Convertor convertor = new MP3Convertor();
+            convertor.ReportingProgress += convertor_ReportingProgress;
+            convertor.WaveToMP3((string)e.Argument);
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            _ProgressDialog.Progress = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            _ProgressDialog.Close();
+
+            if (e.Cancelled)
+                return;
+
+            if (e.Error != null)
+            {
+                string error = Messages.GUIConvertError + Environment.NewLine + e.Error.Message;
+                MessageBox.Show(error, Messages.GUIConvertTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            RefreshRecordings();
+        }
+
+        private void convertor_ReportingProgress(object sender, ConvertorProgressEventArgs e)
+        {
+            backgroundWorker.ReportProgress(e.Progress);
+        }
+    }
+}
